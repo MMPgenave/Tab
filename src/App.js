bedae6c30e257d287b33c4428bbb9ec27836fa26
MMPgenave <mohammadmp14@gmail.com>
2021-08-27 21:35:07 +0430

    building the App( named Tabs)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import "./style.css";
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Tommy from "./tommy";
 
+const url = "https://course-api.com/react-tabs-project";
 function App() {
+  const [isLoading, setIsloading] = useState(true);
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((resp) => resp.data)
+      .catch((err) => console.log(err))
+      .then((DATA) => {
+        console.log(`the data gatherd with axios is ${DATA}`);
+        setData((prevValue) => DATA);
+        setIsloading(false);
 
+        console.log(`the data defined with useState is ${DATA}`);
+      });
+  }, []);
 
-
-
-
-
+  if (isLoading) {
+    return <h1>The page is Loading...</h1>;
+  }
 
   return (
     <div>
@@ -20,14 +38,16 @@ function App() {
               toomy
             </button>
             <button type="button" className="singleBtn">
-              bigrop
+              bigdrop
             </button>
             <button type="button" className="singleBtn">
               cuker
             </button>
           </div>
         </div>
-        <div className="rightDiv">right</div>
+        <div className="rightDiv">
+          <Tommy info={data} />
+        </div>
       </div>
     </div>
   );
---
+++ src/tommy.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Tommy = (prop) => {
+  console.log(`the tommy prop  0 is ${prop.info[0]}`);
+
+  const [dar, setValue] = useState({});
+
+ 
+
+  useEffect(() => {
+    let filteredData = {};
+    for (let i = 0; i < prop.info.length; i++) {
+      if (prop.info[i].company === "TOMMY") {
+        console.log(`${i}Hello`);
+        filteredData = { ...prop.info[i] };
+      }
+    }
+    console.log(`filteredData= ${JSON.stringify(filteredData)}`);
+    setValue((prevState) => filteredData);
+  }, []);
+
+  return (
+    <div>
+      <h1>{dar.title}</h1>
+    </div>
+  );
+};
+
+export default Tommy;
