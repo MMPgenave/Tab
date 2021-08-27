43d3f512ec1d56d1f0f3b85fa98b3c8cbb7a5513
MMPgenave <mohammadmp14@gmail.com>
2021-08-27 21:00:17 +0430

    Consturcing the css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-
+import "./style.css";
 
 function App() {
+
+
+
+
+
+
+
+
   return (
-    <div >
-      <h1>wellcome to React.js !</h1>
+    <div>
+      <h1>Experience</h1>
+      {/*  <div className="underline"></div>  */}
+      <div className="container">
+        <div className="leftDiv">
+          <div className="btn_container">
+            <button type="button" className="singleBtn">
+              toomy
+            </button>
+            <button type="button" className="singleBtn">
+              bigrop
+            </button>
+            <button type="button" className="singleBtn">
+              cuker
+            </button>
+          </div>
+        </div>
+        <div className="rightDiv">right</div>
+      </div>
     </div>
   );
 }
 
 export default App;
-
 
 // App.js => 1.hamma : line: 20-25 -> "hmma message ...";
 //     2.Saheb : linee:20-25 -> "saheb Code ...";
