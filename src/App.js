258f69f37785b3b819e341244ac1ced1b48f79d5
MMPgenave <mohammadmp14@gmail.com>
2021-08-27 18:11:20 +0430

    removing the App.css file and other unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <h1>wellcome to React.js !</h1>
     </div>
   );
 }
 
 export default App;
+
+
+// App.js => 1.hamma : line: 20-25 -> "hmma message ...";
+//     2.Saheb : linee:20-25 -> "saheb Code ...";
